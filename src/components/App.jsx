463e6e94abc9eebf3b220902edaeb1e14618aa37
Kareem Gurung <kareemgurung@Kareems-MacBook-Pro.local>
2020-11-14 12:33:54 +0000

    initial setup

---
+++ src/components/App.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import reviews from '../data';
+
+function App() {
+    return <h3>Hello World</h3>
+}
+
+export default App;
---
+++ src/components/Review.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Review() {
+    return <p>This is a review component.</p>
+}
+
+export default Review;
